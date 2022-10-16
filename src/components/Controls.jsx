a2e52abeb2097eef1ae731a0bfb933d5e97dc8f2
Vladimir <v.a.poleshchuk@yandex.ru>
2022-10-17 01:01:01 +0300

    first commit

---
+++ src/components/Controls.jsx
@@ -0,0 +1,51 @@
+import styled from 'styled-components';
+
+import { Search } from './Search';
+import { CustomSelect } from './CustomSelect';
+import {useDispatch, useSelector} from "react-redux";
+import {selectRegion} from "../store/controls/controls-selectors";
+import {setRegion} from "../store/controls/controls-actions";
+
+const optionsMap = {
+  'Africa': { value: 'Africa', label: 'Africa' },
+  'America': { value: 'America', label: 'America' },
+  'Asia': { value: 'Asia', label: 'Asia' },
+  'Europe': { value: 'Europe', label: 'Europe' },
+  'Oceania': { value: 'Oceania', label: 'Oceania' },
+}
+const options = Object.values(optionsMap);
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+
+  @media (min-width: 767px) {
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  }
+`;
+
+export const Controls = () => {
+    const dispatch = useDispatch()
+    const region = useSelector(selectRegion)
+
+    const handleSelect = (region) => {
+        dispatch(setRegion(region?.value || ''))
+    }
+
+    return (
+        <Wrapper>
+            <Search />
+            <CustomSelect
+                options={options}
+                placeholder="Filter by Region"
+                isClearable
+                isSearchable={false}
+                value={optionsMap[region]}
+                onChange={handleSelect}
+            />
+        </Wrapper>
+    );
+};
